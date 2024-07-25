57638a1c825236f4ab08b5121773c30ca35c20e1
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-07-25 20:20:27 -0300

    feat: colors pallete

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "colorPrimary": "var(--colorPrimary)",
+        "colorSecondary": "var(--colorSecondary)",
+        "colorTerciary": "var(--colorTerciary)",
+        "colorContrast": "var(--colorContrast)",
+        "colorDark": "var(--colorDark)",
+        "colorLight": "var(--colorLight)",
+        "colorBackground": "var(--colorBackground)",
+      }
     },
   },
   plugins: [],
