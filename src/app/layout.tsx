744e3ab34b873f00df856f18035cb2bb5ec2aaaa
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-07-26 19:45:02 -0300

    fix: changing header local import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
+import ThemeProvider from "@/components/ThemeProvider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ThemeProvider from "@/components/ThemeProvider";
-import { Header } from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +19,6 @@ export default function RootLayout({
     <html lang="pt-br">
         <body className={inter.className}>
           <ThemeProvider>
-            <Header />
             {children}
           </ThemeProvider>
         </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { AboutMe } from "@/components/AboutMe";
+import { Header } from "@/components/Header";
 import { HomeContent } from "@/components/HomeContent";
 import { useThemeContext } from "@/hooks/useThemeContext";
 
@@ -6,8 +8,12 @@ export default function Home() {
   const { themeOperator, toggleTheme } = useThemeContext();
   return (
     <>
-      <div className={`${themeOperator} pt-[140px] text-colorDark`}>
-        <HomeContent />
+      <div className={`${themeOperator} text-colorDark`}>
+        <Header />
+        <div className="pt-[140px] px-4">
+          <HomeContent />
+          <AboutMe />
+        </div>
       </div>
     </>
   );
