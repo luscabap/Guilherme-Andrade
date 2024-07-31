27e91abc2d55b8dce6b2c79768ed9877efa1d125
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-07-31 11:46:53 -0300

    feat: adding animation on ModalWhatsApp | fix: fixing ModalWhatsapp position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,11 @@ import { HomeContent } from "@/components/HomeContent";
 import { Introduction } from "@/components/Introduction";
 import { ModalWhatsapp } from "@/components/ModalWhatsapp";
 import { useThemeContext } from "@/hooks/useThemeContext";
+import { AnimatePresence } from "framer-motion";
 import { useState } from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
+import { motion } from "framer-motion"
 
 export default function Home() {
   const { themeOperator, toggleTheme } = useThemeContext();
@@ -21,13 +23,16 @@ export default function Home() {
     setModalWPIsOpen(prevValue => !prevValue)
   }
 
+
   return (
     <>
       <div className={`${themeOperator} text-colorFont bg-colorBackground`}>
-          <div className="rounded-full bg-colorPrimary absolute bottom-2 right-2 p-2">
-            {modalWPIsOpen ? <IoMdClose className="w-14 h-14" onClick={toggleModalWP}/> : <FaWhatsapp className="w-14 h-14" onClick={toggleModalWP}/>}
+          <div className="rounded-full bg-colorPrimary bottom-2 right-2 p-2 fixed z-50">
+              {modalWPIsOpen ? <IoMdClose className="w-14 h-14" onClick={toggleModalWP}/> : <FaWhatsapp className="w-14 h-14" onClick={toggleModalWP}/>}
           </div>
+        <AnimatePresence>
           {modalWPIsOpen && <ModalWhatsapp />}
+        </AnimatePresence>
         <Header />
         <div className="pt-[160px] px-4 flex flex-col gap-12">
           <Introduction />
--- src/components/ModalWhatsapp/index.tsx
+++ src/components/ModalWhatsapp/index.tsx
@@ -1,10 +1,28 @@
-import { ButtonContact } from "../ButtonContact"
+import { ButtonContact } from "../ButtonContact";
+import { motion, AnimationType } from "framer-motion"
 
 export const ModalWhatsapp = () => {
+  const modalVariants = {
+    initial: {
+      scaleY: 0
+    },
+    animate: {
+      scaleY: 1,
+    },
+    exit: {
+      scaleY: 0
+    }
+  }
+
   return (
-    <div className="absolute right-2 bottom-24 bg-colorPrimary py-4 px-2 flex flex-col items-center justify-center gap-5 rounded-xl">
+    <motion.div 
+      variants={modalVariants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      className="fixed right-2 bottom-24 bg-colorPrimary py-4 px-2 flex flex-col items-center justify-center gap-5 rounded-xl z-50 origin-bottom-right">
       <h5>Clique aqui para falar com um especialista!</h5>
       <ButtonContact />
-    </div>
+    </motion.div>
   )
 }
