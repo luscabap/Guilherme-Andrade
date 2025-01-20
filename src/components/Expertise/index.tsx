ef2e2ed427b1c602c4bb93b8c66c6ab36472d1d6
luscabap <lucasbaptistasilva.dev@gmail.com>
2025-01-20 19:38:23 -0300

    formating

--- src/components/Expertise/index.tsx
+++ src/components/Expertise/index.tsx
@@ -5,12 +5,18 @@ import { TitleSection } from "../TitleSection";
 export const Expertise = () => {
   return (
     <div id="area-atuacao">
-      <TitleSection texto="Área de atuação"/>
+      <TitleSection texto="Área de atuação" />
       <div className="flex flex-col gap-8 my-4 items-center justify-center 2xl:flex-row flex-wrap xl:gap-12">
-        { dataItems.map(item => (
-          <ItemDescriptive key={item.id} description={item.description} icon={<item.icon className="text-colorPrimary" size={35}/>} title={item.title} primary={item.primary}/>
-        )) }
+        {dataItems.map((item) => (
+          <ItemDescriptive
+            key={item.id}
+            description={item.description}
+            icon={<item.icon className="text-colorPrimary" size={35} />}
+            title={item.title}
+            primary={item.primary}
+          />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
